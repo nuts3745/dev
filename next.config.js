a8db5c318f5a276246f73d92e59231fadb1ecbe2
nuts3745 <japanbik@gmail.com>
2021-09-03 00:39:26 +0900

    meow

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+};
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-blog",
+  themeConfig: "./theme.config.js",
+});
+module.exports = withNextra();
---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+
+import type { AppProps } from "next/app";
+import "nextra-theme-blog/style.css";
+import Head from "next/head";
+
+export default function Nextra({ Component, pageProps }: AppProps) {
+      return (
+    <>
+      <Head>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS"
+          href="/feed.xml"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Inter-roman.latin.var.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
