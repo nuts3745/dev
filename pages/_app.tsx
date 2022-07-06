2334b9c1c4fde213e1e83fcd5a97f46bdf49029c
nuts3745 <japanbik@gmail.com>
2022-07-06 12:50:12 +0900

    change main menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import 'nextra-theme-blog/style.css'
+import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }: AppProps) {
   return (
